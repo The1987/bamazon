cc56c298170268aa1525074a84f043516b340cd5
you <you@yous-MacBook.home>
2018-05-13 22:59:47 -0400

    uploading assignment

---
+++ bamazonSeeds.sql
@@ -0,0 +1,33 @@
+/* Seeds for SQL table. We haven't discussed this type of file yet */
+USE bamazon_db;
+
+/* Insert 3 Rows into your new table */
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Sony TV", "Electronics", 1999, 100);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Toshiba TV", "Electronics", 1799, 90);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Apple TV", "Electronics", 199, 80);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Apple Watch", "Electronics", 159, 10);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("FitBit Watch", "Electronics", 179, 60);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Garmon Watch", "Electronics", 149, 100);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("iPhone 4", "Electronics", 279, 80);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("iPhone 10", "Electronics", 999, 10);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Android", "Electronics", 399, 20);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Samsung TV", "Electronics", 599, 4);
